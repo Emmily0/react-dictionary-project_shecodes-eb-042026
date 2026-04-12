fd8aee96441d22401095d9967e19a3c2839270fe
Emmily0 <81269634+Emmily0@users.noreply.github.com>
2026-04-12 23:11:18 +0200

    Added the Search Engine Keyword form + useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="container">
+    <header className="App-header">
     <h1>Dictionary</h1>
+    </header>
+    <main>
+      <Dictionary />
+    </main>
+    <footer className="App-footer">
+      <small>Coded by Emily</small>
+    </footer>
     </div>
   );
 }
