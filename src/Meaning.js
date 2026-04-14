306efecff79318fd7d8e07c45fcd9889328dd7e1
Emmily0 <81269634+Emmily0@users.noreply.github.com>
2026-04-14 22:49:28 +0200

    Added Synonyms file & Component + added titles in Meaning def & ex

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Meaning.css";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
 
     return (
         <div className="Meaning">
             <h3 className="partOfSpeech">{props.meaning.partOfSpeech}</h3>
-            <p className="defintion">{props.meaning.definition} </p>
-            <span className="example">{props.meaning.example && <em>"{props.meaning.example}"</em>}</span>
+            <p className="definition">Definition: {props.meaning.definition} </p>
+            <span className="example">{props.meaning.example && <em>Example: "{props.meaning.example}"</em>}</span>
+            <Synonyms synonyms={props.meaning.synonyms} />
         </div>
     );
 }
